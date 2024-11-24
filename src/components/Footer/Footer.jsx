45fdfa012b00fa99d68c4a3bfd32607f2fbbf5fd
Shashi Kumar <guptashashi986@gmail.com>
2024-11-24 18:09:18 +0530

    Done!

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {FaFacebookF,FaInstagram,FaLinkedinIn, FaTelegram} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className='bg-gradient-to-r from-primary to-secondary pt-12 pb-8 text-white'>
+        <div className='container'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-white'>
+                {/* company details section */}
+                <div data-aos="fade-up" className='space-y-6'>
+                    <h1 className='text-4xl font-bold'>OrangeMint</h1>
+                    <p className='text-sm max-w-[300px]'>
+                        Lorem, ipsum dolor sit amet consectetur 
+                        adipisicing elit. Possimus maxime sed aperiam 
+                        perspiciatis optio doloremque nostrum ea laborum sint, at 
+                        beatae nobis eligendi vitae! Molestias.
+                    </p>
+                </div>
+
+                {/* Navlink section */}
+                <div data-aos="fade-up" data-aos-delay="300" className='space-y-6'>
+                    <h1 className='text-4xl font-bold'>Quick Links</h1>
+                    <div className='grid grid-cols-2 gap-3'>
+                        {/* first column  links*/}
+                        <div>
+                            <ul className='space-y-2'>
+                               <li><a href='#'>Home</a></li> 
+                               <li><a href='#'>About</a></li> 
+                               <li><a href='#'>Contact</a></li> 
+                               <li><a href='#'>Search Fruits</a></li> 
+                               <li><a href='#'>Privacy Policy</a></li> 
+                            </ul>
+                        </div>
+                        {/* second column links*/}
+                        <div>
+                            <ul className='space-y-2'>
+                               <li><a href='#'>Home</a></li> 
+                               <li><a href='#'>About</a></li> 
+                               <li><a href='#'>Contact</a></li> 
+                               <li><a href='#'>Search Fruits</a></li> 
+                               <li><a href='#'>Privacy Policy</a></li> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                {/* social links section */}
+                <div data-aos="fade-up" data-aos-delay="500" className='space-y-6'>
+                    <h1 className='text-4xl font-bold'>
+                        Follow Us
+                    </h1>
+                    <div>
+                    <p>+1 (123) 456-7890</p>
+                    <p>Kolkata,West Bengal</p>
+                    </div>
+                    <div className='flex items-center gap-3'>
+                    <FaFacebookF className='text-3xl hover:scale-105 duration-300'/>
+                    <FaInstagram className='text-3xl hover:scale-105 duration-300' />
+                    <FaLinkedinIn className='text-3xl hover:scale-105 duration-300'/>
+                    <FaTelegram className='text-3xl hover:scale-105 duration-300'/>
+                    </div>
+                </div>
+                
+            </div>
+            {/* copyright section */}
+            <p className='text-white text-center mt-8 pt-8 border-t-2'>
+                Copyright &copy; 2024 Company Name. All rights reserved.
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
